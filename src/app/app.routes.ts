d3b45541ba98ea1dc8d8c22d2a044a3dc4994ac2
Wanjie Ryan <ryanwanjiedistortion@gmail.com>
2026-09-23 13:20:39 +0300

    angular startup & API calls

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
-import { Login } from './pages/login/login'
+import { Login } from './pages/login/login';
 import { authGuard } from './guards/auth-guard';
+import { Dashboard } from './pages/dashboard/dashboard';
 
 export const routes: Routes = [
-    {path:'', redirectTo:'login', pathMatch:'full'},
-    {path:'login', component:Login}
-    {path: 'dashboard', component: Dashboard, canActivate: [authGuard]}
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  { path: 'dashboard', component: Dashboard, canActivate: [authGuard] },
 ];
--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Api, Product } from '../../services/api';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,4 +11,55 @@ import { ReactiveFormsModule } from '@angular/forms';
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
-export class Dashboard {}
+export class Dashboard implements OnInit {
+  private api = inject(Api);
+  private fb = inject(FormBuilder);
+  private toastr = inject(ToastrService);
+  private router = inject(Router);
+
+  protected products = signal<Product[]>([]);
+  protected isLoading = signal(true);
+  protected isSubmitting = signal(false);
+
+  productForm = this.fb.group({
+    title: ['', Validators.required],
+    price: [0, [Validators.required, Validators.min(1)]],
+  });
+
+  ngOnInit() {
+    this.api
+      .getProducts()
+      .pipe(finalize(() => this.isLoading.set(false)))
+      .subscribe({
+        next: (res) => this.products.set(res.products),
+        error: () => this.toastr.error('Could not load products', 'Load Failed'),
+      });
+  }
+
+  onSubmit() {
+    if (this.productForm.invalid) {
+      this.productForm.markAllAsTouched();
+      return;
+    }
+
+    this.isSubmitting.set(true);
+
+    const { title, price } = this.productForm.value;
+
+    this.api
+      .addProduct(title!, price!)
+      .pipe(finalize(() => this.isSubmitting.set(false)))
+      .subscribe({
+        next: (newProduct) => {
+          this.products.update((list) => [newProduct, ...list]);
+          this.toastr.success('Product added');
+          this.productForm.reset();
+        },
+        error: () => this.toastr.error('Could not add product', 'Submit Failed'),
+      });
+  }
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
+}
